7e1007a5c90e9c0d0455b880870f86fbacf72afc
Oliver <oliver.vukovic@quantoxtechnology.com>
2021-10-06 18:18:29 +0200

    html & css modification + new js

--- starter-code/w5-script.js
+++ starter-code/w5-script.js
@@ -1,5 +1,144 @@
 
 
+/* kreirati funkciju za fetch podataka sa gitHub-a */
+
+const months = [
+  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+]
+
+
+function fetchApi(username) {
+  
+  fetch('https://api.github.com/users/${username}', {headers:{'Authorization':'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'}})
+  .then((response) => {
+    
+
+
+    if(response.status === 404) {
+      document.getElementById("result-none").innerHTML = "No results"
+      document.getElementById("result-none").style.visibility = "visible"
+      fetchApi("octocat")
+    } else {
+      if(username !== "octocat") {
+        document.getElementById("result-none").style.visibility = "hidden"
+      }
+      return response.json()
+    }
+  })
+  .then((data) => {
+      document.getElementById("profil-img").src = `${data.avatar_url}`;
+      document.getElementById("username").innerHTML = `@${data.login}`;
+      document.getElementById("repos").innerHTML = `${data.public_repos}`;
+      document.getElementById("followers").innerHTML = `${data.followers}`;
+      document.getElementById("following").innerHTML = `${data.following}`;
+
+      if(data.username) {
+        document.getElementById("name").innerHTML = '${data.name}'
+      } else {
+        document.getElementById("name").innerHTML = '${data.login}'
+      }
+
+      if(data.bio) {
+        document.getElementById("bio-text").innerHTML = `${data.profil-text}`
+        document.getElementById("bio-text").classList.remove("transparent-bio")
+      } else {
+        document.getElementById("bio-text").innerHTML = `This profile has no bio`
+        document.getElementById("bio-text").classList.add("transparent-bio")
+      }
+
+      if(data.twitter_username) {
+        document.getElementById("twitter").innerHTML = `<a target="_blank" 
+         href="https://mobile.twitter.com/${data.twitter_username}">${data.twitter_username}
+         </a>`
+        document.getElementById("twitter").classList.remove("transparent-info")
+      } else {
+        document.getElementById("twitter").innerHTML = `Not available`
+        document.getElementById("twitter").classList.add("transparent-info")
+        document.getElementById("img-twitter").classList.add("transparent-image")
+      }
+
+      if(data.location) {
+        document.getElementById("location").innerHTML = '${data.locaton}'
+        document.getElementById("location").classList.remove("transparent-info")
+      } else {
+        document.getElementById("location").innerHTML = `Not available`
+        document.getElementById("location").classList.add("transparent-info")
+        document.getElementById("img-location").classList.add("transparent-image")
+      }
+
+      if(data.blog){
+        document.getElementById("website").innerHTML = `<a target="_blank" href="${data.blog}">${data.blog}</a>`
+        document.getElementById("website").classList.remove("transparent-info")
+     } else {
+
+        document.getElementById("website").innerHTML = `Not available`
+        document.getElementById("website").classList.add("transparent-info")
+        document.getElementById("img-website").classList.add("transparent-image")
+     }
+
+     if(data.company) {
+      company = data.company.slice(1)
+      document.getElementById("company").innerHTML = `<a target="_blznk" href="https://github.com/${company}">${data.company}</a>` 
+      document.getElementById("company").classList.remove("transparent-info")
+      
+   } else {
+      document.getElementById("company").innerHTML = `Not available`
+      document.getElementById("company").classList.add("transparent-info")
+      document.getElementById("img-company").classList.add("transparent-image")
+   }
+
+     let date = new Date(data.created_at)
+      document.getElementById("date").innerHTML = 
+      `Joined ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+  })
+}
+
+
+
+/*
+   Default loading JS
+*/
+fetchApi("octocat")
+
+/*
+   nakon submitovanja username-a
+*/
+let form = document.getElementById("search-form")
+form.addEventListener('submit', function(e){
+   
+   e.preventDefault();
+
+   let searchName = document.getElementById("search-name").value;
+   if (searchName === "") {
+
+      if (document.getElementById("result-none").style.visibility === "visible") 
+         document.getElementById("result-none").style.visibility = "hidden"
+      return
+   } else if (searchName.startsWith("@")) {
+
+      document.getElementById("result-none").innerHTML = "Remove \"@\""
+      document.getElementById("result-none").style.visibility = "visible"
+   } else {
+
+      fetchApi(searchName)
+   }
+   
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var r = document.querySelector(':root');   //root element
 
 /*function colorFunction() {                 //funkcija za dobijanje promenjive vrednosti
@@ -8,6 +147,7 @@ var r = document.querySelector(':root');   //root element
 
     alert("Value is " + rStyle.getPropertyValue('black'));    //alert
 }*/
+
 
 function changeColor() {           //funkcija za podesavanje promenljiviv vrednosti
 
@@ -27,8 +167,8 @@ function changeColor() {           //funkcija za podesavanje promenljiviv vredno
     r.style.setProperty('--colorimg', 'brightness(5)');
 }
 
-/*
-function changeColor() {
+
+function changeButton() {
     var x = document.getElementById("dark");
     var y = document.getElementById("light");
     if (x.style.display === "none") {
@@ -39,7 +179,7 @@ function changeColor() {
     }
   }
   
-  function returnColor() {
+  function returnButton() {
     var y = document.getElementById("light");
      var x = document.getElementById("dark");
     if (y.style.display === "none") {
@@ -50,7 +190,7 @@ function changeColor() {
     }
   }
 
-*/
+
 
 
 /*
@@ -59,7 +199,13 @@ let darkInput = document.getElementById("dark").value;
         document.getElementById
     }
 
+-----------------
+function changeColor() {
+    var element = document.body;
+    element.clssList.toggle("dark-screen")
+}/*
 
+------------------   u css / .dark-screen { background-color:black; color-white;}
 
 function returnColor() {
     return;
